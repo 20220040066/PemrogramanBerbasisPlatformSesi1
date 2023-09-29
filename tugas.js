4fc175116ca9367e84987aa08df5e5721c3f8089
Silvaaa <124055481+20220040066@users.noreply.github.com>
2023-09-29 20:30:14 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama ='silva destiani'
+let nim = '20220040066'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+//aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 20;
+let b = 12;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +(a+b))
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' + (a-b))
